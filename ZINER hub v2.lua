a1a297d2178d9edac4d81a9bd5a27a69938db89f
ATOMIC KING <157969330+Tienvn123tkvn@users.noreply.github.com>
2024-05-05 11:21:58 +0700

    Update ZINER hub v2.lua

--- ZINER hub v2.lua
+++ ZINER hub v2.lua
@@ -2504,7 +2504,7 @@ ImageButton.BorderSizePixel = 0
 ImageButton.Position = UDim2.new(0.120833337, 0, 0.0952890813, 0)
 ImageButton.Size = UDim2.new(0, 50, 0, 50)
 ImageButton.Draggable = true
-ImageButton.Image = "rbxassetid://http://www.roblox.com/asset/?id=17382863073"
+ImageButton.Image = "rbxassetid://17382863073"
 ImageButton.MouseButton1Down:connect(function()
     game:GetService("VirtualInputManager"):SendKeyEvent(true,Enum.KeyCode.End,false,game)
 end)
